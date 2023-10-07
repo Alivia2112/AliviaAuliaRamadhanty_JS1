4de75610f0efd584997118daba2422790df16b41
Alivia2112 <aliviarevinaldi21@gmail.com>
2023-10-07 16:55:30 +0700

    Tugas JS 1

---
+++ script.js
@@ -0,0 +1,75 @@
+/* if, else
+
+let umur = prompt("berapa umur gracia?");
+
+if (umur === 24) {
+    alert("benar");
+} else {
+    alert("bukan umur gracia");
+} */
+
+/* nested if
+
+let makan = prompt("udah makan belum?");
+
+if (makan === "udah") {
+    alert("Pinter anak mama udah mam");
+  } else if (makan === "belum") {
+    alert("Kok belum makan?");
+  } else {
+    alert("udah apa belum?");
+  } */
+
+  /* switch case
+
+  let memberjeketi = prompt("masukkan nama member jkt48")
+
+  switch (memberjeketi) {
+    case "shani":
+      alert("shani adalah kapten jkt48");
+      break;
+    case "jinan":
+      alert("jinan adalah wakil kapten jkt48");
+      break;
+    case "gracia":
+      alert("gracia adalah member jkt48 generasi 3");
+      break;
+    default:
+      alert("bukan member jkt48");
+      alert("masukkan nama lain");
+  } */
+
+  /* for statement
+
+for ( let i = 0; i <= 10; i++) {
+    console.log("ini angka " + i);
+} */
+
+/* while
+
+let angka = 0;
+
+while (angka <= 10) {
+    console.log("kendaraan" + angka);
+    angka++;
+} */
+
+/* do while
+
+let i = 0;
+do {
+    i += 1;
+    console.log("mobil" + i);
+} while (i <15);
+*/
+
+/* function
+
+let jumlahDuaBilangan = function (a, b) {
+   let jumlah;
+   jumlah = a + b;
+
+    return jumlah;
+}
+console.log(jumlahDuaBilangan (20, 25));
+*/
